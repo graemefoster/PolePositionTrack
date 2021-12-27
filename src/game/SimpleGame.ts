746779966fcaca5ddad5b5ab752ad819c45dfe9f
Graeme Foster <graemefoster@gmail.com>
2021-12-27 20:27:00 +0800

    First commit

---
+++ src/game/SimpleGame.ts
@@ -0,0 +1,141 @@
+//                    o
+//        |           o 
+//   -    |            
+//        |           
+//                    o
+//                    o
+//   <----><---------->
+//     c         d
+//
+//   - === camera
+//   | === screen (-1 -> 1)
+//   o === points to project onto screen from camera
+//   c === length from camera to screen
+//   d === distance from screen to earth
+
+export interface Point {
+    x: number
+    y: number
+    z: number
+}
+
+export interface Point2D {
+    x: number
+    y: number
+}
+
+export class Camera {
+
+    x: number
+    y: number
+    z: number
+
+    constructor(x: number, y: number, z: number) {
+        this.x = x
+        this.y = y
+        this.z = z
+    }
+
+    projectPoint(point: Point, plane: ProjectionPlane): Point2D {
+
+        let adjacent = this.z - point.z
+        let otherAdjacent = plane.z - this.z
+
+        let yOpposite = ((this.y - point.y) * otherAdjacent) / adjacent
+        let pointOnProjectionPlaneY = this.y + yOpposite
+
+        let xOpposite = ((this.x - point.x) * otherAdjacent) / adjacent
+        let pointOnProjectionPlaneX = this.x + xOpposite
+
+        console.log(`Original ${point.x}, ${point.y}: Point on plane: ${xOpposite}, ${yOpposite}`)
+
+        return {x:pointOnProjectionPlaneX, y:pointOnProjectionPlaneY}
+
+    }
+
+
+}
+
+export class ProjectionPlane {
+    z: number
+    
+    constructor(z: number) {
+        this.z = z
+    }
+}
+
+export class ScreenProjection {
+
+    x1: number
+    x2: number
+    y1: number
+    y2: number
+    width: number
+    height: number
+    screenPixelsPerWorldPixelX: number
+    screenPixelsPerWorldPixelY: number
+    
+    constructor(x1: number, x2: number, y1: number, y2: number, width: number, height: number) {
+        this.x1 = x1
+        this.x2 = x2
+        this.y1 = y1
+        this.y2 = y2
+        this.width = width
+        this.height = height
+        this.screenPixelsPerWorldPixelX = width / (x2 - x1)
+        this.screenPixelsPerWorldPixelY = height / (y2 - y1)
+    }
+
+    project(point: Point2D) : Point2D {
+        const worldXOffsetOnScreen = point.x - this.x1
+        const worldYOffsetOnScreen = point.y - this.y1
+        console.log(`world offset Y:${worldYOffsetOnScreen}`)
+        console.log(`pixels per Y:${this.screenPixelsPerWorldPixelY}`)
+        return  {
+            x: worldXOffsetOnScreen * this.screenPixelsPerWorldPixelX,
+            y: worldYOffsetOnScreen * this.screenPixelsPerWorldPixelY
+        }
+    }
+}
+
+export interface Drawable {
+    render(canvas: CanvasRenderingContext2D, camera: Camera, plane: ProjectionPlane): void
+}
+
+export class RoadSegment implements Drawable {
+
+    xCentre: number
+    width: number
+    height: number
+    distance: number
+
+    constructor(xCentre: number, width: number, height: number, distance: number) {
+        this.xCentre = xCentre
+        this.width = width
+        this.height = height
+        this.distance = distance
+    }
+    
+    render(canvas: CanvasRenderingContext2D, camera: Camera, plane: ProjectionPlane): void {
+        const point1: Point2D = camera.projectPoint({
+            x: this.xCentre - (this.width / 2),
+            y: this.height,
+            z: this.distance
+        }, plane)
+
+        const point2 = camera.projectPoint({
+            x: this.xCentre + (this.width / 2),
+            y: this.height,
+            z: this.distance
+        }, plane)
+
+        canvas.beginPath()
+        canvas.moveTo(point1.x, point1.y)
+        canvas.lineTo(point2.x, point2.y)
+        canvas.closePath()
+        canvas.stroke()
+
+        console.log(`${point1.x},${point1.y} -> ${point2.x},${point2.y}`)
+
+    }
+}
---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { Camera, Point, Point2D, ProjectionPlane, RoadSegment } from './game/SimpleGame'
+
+
+const roadSegments: RoadSegment[] = [
+    new RoadSegment(500, 500, 300, 10),
+    new RoadSegment(500, 500, 300, 20),
+    new RoadSegment(500, 500, 300, 30),
+    new RoadSegment(500, 400, 300, 40),
+    new RoadSegment(500, 500, 290, 50),
+    new RoadSegment(510, 500, 290, 60),
+    new RoadSegment(530, 500, 300, 70),
+    new RoadSegment(530, 500, 300, 80)
+]
+
+const _global = (window /* browser */ || global /* node */) as any
+_global.drawScene = function drawScene(x: number, y: number, d: number, dp: number) {
+    const camera = new Camera(x, y, d)
+    const plane: ProjectionPlane = new ProjectionPlane(10) //300, 600,0, 200, dp, 800.0, 600.0)
+
+    const canvas = document.getElementById('game') as HTMLCanvasElement
+    const canvasContext = canvas.getContext("2d")!
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height)
+    roadSegments.forEach(s => {
+        s.render(canvasContext, camera, plane)
+    })
+}
